c9036cf24c06614282c0834b3df6bb5fef11d618
mkhai207 <lkhai4617@gmail.com>
2025-09-26 20:30:04 +0700

    fix connect postgres build render

--- src/config/postgres.js
+++ src/config/postgres.js
@@ -22,7 +22,11 @@ let client;
 	const configObj = parse(config.sqlDB.connectionString);
 
 	client = new Client({
-		...configObj,
+		user: configObj.user,
+		password: configObj.password,
+		host: configObj.host,
+		port: parseInt(configObj.port, 10),
+		database: configObj.database,
 		ssl: { rejectUnauthorized: false },
 		lookup: (hostname, opts, cb) => {
 			dns.lookup(hostname, { family: 4 }, cb);
