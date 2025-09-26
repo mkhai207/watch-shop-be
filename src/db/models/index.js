9582294593d78418c77e61397a4ca00cf82e5e52
mkhai207 <lkhai4617@gmail.com>
2025-09-26 23:37:18 +0700

    fix

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -1,51 +1,3 @@
-// /* eslint-disable global-require */
-// /* eslint-disable import/no-dynamic-require */
-// const fs = require('fs');
-// const path = require('path');
-// const Sequelize = require('sequelize');
-
-// const config = require(`${__dirname}/../../config/config`);
-
-// const basename = path.basename(module.filename);
-
-// const db = {};
-
-// const sequelize = new Sequelize(
-// 	config.sqlDB.database,
-// 	config.sqlDB.user,
-// 	config.sqlDB.password,
-// 	{
-// 		...config.sqlDB,
-// 		logging: false,
-// 	}
-// );
-
-// fs.readdirSync(__dirname)
-// 	.filter(
-// 		(file) =>
-// 			file.indexOf('.') !== 0 &&
-// 			file !== basename &&
-// 			file.slice(-9) === '.model.js'
-// 	)
-// 	.forEach((file) => {
-// 		const model = require(path.join(__dirname, file))(
-// 			sequelize,
-// 			Sequelize.DataTypes
-// 		);
-// 		db[model.name] = model;
-// 	});
-
-// Object.keys(db).forEach((modelName) => {
-// 	if (db[modelName].associate) {
-// 		db[modelName].associate(db);
-// 	}
-// });
-
-// db.sequelize = sequelize;
-// db.Sequelize = Sequelize;
-
-// module.exports = db;
-
 /* eslint-disable global-require */
 /* eslint-disable import/no-dynamic-require */
 const fs = require('fs');
@@ -58,18 +10,38 @@ const basename = path.basename(module.filename);
 
 const db = {};
 
-const { connectionString, database, user, password, ...sequelizeOptions } =
-	config.sqlDB;
+const {
+	connectionString,
+	database,
+	user,
+	password,
+	host,
+	dialect,
+	pool,
+	define,
+} = config.sqlDB;
 
-const sequelize = connectionString
-	? new Sequelize(connectionString, {
-			...sequelizeOptions,
-			logging: false,
-	  })
-	: new Sequelize(database, user, password, {
-			...sequelizeOptions,
-			logging: false,
-	  });
+const baseOptions = {
+	dialect,
+	pool,
+	define,
+	logging: false,
+};
+
+let sequelize;
+
+if (connectionString) {
+	baseOptions.dialectOptions = {
+		...baseOptions.dialectOptions,
+		ssl: { require: true, rejectUnauthorized: false },
+	};
+	sequelize = new Sequelize(connectionString, baseOptions);
+} else {
+	sequelize = new Sequelize(database, user, password, {
+		...baseOptions,
+		host,
+	});
+}
 
 fs.readdirSync(__dirname)
 	.filter(
