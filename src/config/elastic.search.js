89eefc679785bf0b9003e632a6080cd5fea45e96
mkhai207 <lkhai4617@gmail.com>
2025-11-07 21:54:14 +0700

    fix: Tắt sniffing của Elasticsearch client

--- src/config/elastic.search.js
+++ src/config/elastic.search.js
@@ -6,6 +6,8 @@ const { node, username, password } = config.elasticSearch;
 const client = new Client({
 	node,
 	...(username && password ? { auth: { username, password } } : {}),
+	sniffOnStart: false,
+	sniffOnConnectionFault: false,
 });
 
 module.exports = client;
