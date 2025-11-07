beee67996b3480a61269a327c74dc592be785ea3
mkhai207 <lkhai4617@gmail.com>
2025-11-07 22:04:38 +0700

    feat: chuyển sang OpenSearch client

--- src/config/elastic.search.js
+++ src/config/elastic.search.js
@@ -1,4 +1,19 @@
-const { Client } = require('@elastic/elasticsearch');
+// const { Client } = require('@elastic/elasticsearch');
+// const config = require('./config');
+
+// const { node, username, password } = config.elasticSearch;
+
+// const client = new Client({
+// 	node,
+// 	...(username && password ? { auth: { username, password } } : {}),
+// 	sniffOnStart: false,
+// 	sniffOnConnectionFault: false,
+// });
+
+// module.exports = client;
+
+const { Client } = require('@opensearch-project/opensearch-js');
+
 const config = require('./config');
 
 const { node, username, password } = config.elasticSearch;
@@ -6,8 +21,6 @@ const { node, username, password } = config.elasticSearch;
 const client = new Client({
 	node,
 	...(username && password ? { auth: { username, password } } : {}),
-	sniffOnStart: false,
-	sniffOnConnectionFault: false,
 });
 
 module.exports = client;
