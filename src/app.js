d7a11cb3834741d9da5c3cdc83246d74371c3515
mkhai207 <lkhai4617@gmail.com>
2025-11-09 08:32:15 +0700

    fix: cors

--- src/app.js
+++ src/app.js
@@ -50,7 +50,7 @@ const corsOptions = {
 	credentials: true,
 };
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+// app.options('*', cors(corsOptions));
 
 app.use(cookieParser());
 
