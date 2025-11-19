f2b6646ed8aeecfb1fed8bd70d2571e93fcecec4
mkhai207 <lkhai4617@gmail.com>
2025-11-19 14:01:14 +0700

    fix user order

--- src/routes/v1/order.route.js
+++ src/routes/v1/order.route.js
@@ -10,57 +10,49 @@ const router = express.Router();
 router
 	.route('/')
 	.get(
-		grantAccess('readAny', resources.ORDER),
+		// grantAccess('readAny', resources.ORDER),
 		validate(orderValidation.getOrders),
 		orderController.getOrders
 	)
 	.post(
-		grantAccess('createAny', resources.ORDER),
+		// grantAccess('createAny', resources.ORDER),
 		validate(orderValidation.createOrder),
 		orderController.createOrder
 	);
 
-router
-	.route('/all')
-	.get(
-		grantAccess('readAny', resources.ORDER),
-		validate(orderValidation.getOrders),
-		orderController.getAllOrders
-	);
+router.route('/all').get(
+	// grantAccess('readAny', resources.ORDER),
+	validate(orderValidation.getOrders),
+	orderController.getAllOrders
+);
 
 router
 	.route('/:orderId')
 	.get(
-		grantAccess('readAny', resources.ORDER),
+		// grantAccess('readAny', resources.ORDER),
 		validate(orderValidation.getOrder),
 		orderController.getOrder
 	)
 	.delete(
-		grantAccess('deleteAny', resources.ORDER),
+		// grantAccess('deleteAny', resources.ORDER),
 		validate(orderValidation.deleteOrder),
 		orderController.deleteOrder
 	);
-router
-	.route('/:orderId/retry-payment')
-	.get(
-		grantAccess('updateAny', resources.ORDER),
-		validate(orderValidation.retryPayment),
-		orderController.retryPayment
-	);
-router
-	.route('/:orderId/change-status')
-	.put(
-		grantAccess('updateAny', resources.ORDER),
-		validate(orderValidation.changeOrderStatus),
-		orderController.changeOrderStatus
-	);
+router.route('/:orderId/retry-payment').get(
+	// grantAccess('updateAny', resources.ORDER),
+	validate(orderValidation.retryPayment),
+	orderController.retryPayment
+);
+router.route('/:orderId/change-status').put(
+	// grantAccess('updateAny', resources.ORDER),
+	validate(orderValidation.changeOrderStatus),
+	orderController.changeOrderStatus
+);
 
-router
-	.route('/:orderId/cancel')
-	.put(
-		grantAccess('updateAny', resources.ORDER),
-		validate(orderValidation.cancelOrder),
-		orderController.cancelOrder
-	);
+router.route('/:orderId/cancel').put(
+	// grantAccess('updateAny', resources.ORDER),
+	validate(orderValidation.cancelOrder),
+	orderController.cancelOrder
+);
 
 module.exports = router;
